20f9490dadf0056cabcdc3aabe84665d77f3d551
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-04-26 00:48:31 -0700

    Corregido con U pero no da resultados, probablemente error de memoria o algo

--- Trascequation/partraseq.cpp
+++ Trascequation/partraseq.cpp
@@ -7,9 +7,9 @@
 double const hb = 1.054571818;
 
 //Constantes del problema:
-double const L = 0.25; //Unidades en Amstrongs
-double const a = 100.0; //Unidades en Amstrongs
-double const U = 24.0; //Energía en MeV
+double const L = 0.5; //Unidades en Amstrongs
+double const a = 1.0; //Unidades en Amstrongs
+double const U = 25.0; //Energía en MeV
 
 int const Npasos=1000;
 
@@ -21,18 +21,21 @@ int main ()
   std::cout.precision(10);
   std::cout.setf(std::ios::scientific);
 
-  double E=0.0; //Energía en MeV
-  double dE=0.0001;
+  double E=0.1; //Energía en MeV
+  double dE=0.1;
   double k=sqrt(E);
-  double beta=sqrt(1-E);
- 
-  while (E<1)
+  double beta=sqrt(U-E);
+  double eps=0.0;
+  
+  do
     {
       std::cout<<E<<"\t"<<eqtan(E)<<"\t"<<eqtanh(E)<<"\n";
       E+=dE;
       k=sqrt(E);
-      beta=sqrt(1-E);
+      beta=sqrt(U-E);
+      eps=eqtan(E)-eqtanh(E);
     }
+  while (abs(eps)>=1e-05);
   return 0;
   
 }
@@ -41,14 +44,14 @@ double  eqtan (double E)
 {
   double eq1=0.0;
   double rE=sqrt(E);
-  eq1=-(1.0/rE)*tan((a*rE)/2);
+  eq1=-(1.0/rE)*tan((a*rE)/2.0);
   return eq1;
 }
 
 double eqtanh (double E)
 {
   double eq2=0.0;
-  double u=sqrt(1-E);
-  eq2=(1.0/u)*tanh((u*a)/4);
+  double m=sqrt(U-E);
+  eq2=(1.0/m)*tanh((m*a)/4.0);
   return eq2;
 }

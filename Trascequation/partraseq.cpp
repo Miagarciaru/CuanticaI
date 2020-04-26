5486c031a58d9fc7558e2b1dd4b1f4cae3be1838
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-04-26 12:57:26 -0700

    Da buena gráfica en tan(E) pero valores pequeños de tanh(E), ya estaba con hb=1

--- Trascequation/partraseq.cpp
+++ Trascequation/partraseq.cpp
@@ -4,14 +4,13 @@
 #include <cmath>
 
 //Constantantes físicas:
-double const hb = 1.054571818;
+double const hb = 1.0;
 
 //Constantes del problema:
 double const L = 0.5; //Unidades en Amstrongs
 double const a = 1.0; //Unidades en Amstrongs
 double const U = 25.0; //Energía en MeV
-
-int const Npasos=1000;
+double const m = 1.0; //Masas del electron
 
 double  eqtan (double k);
 double eqtanh (double beta);
@@ -21,21 +20,17 @@ int main ()
   std::cout.precision(10);
   std::cout.setf(std::ios::scientific);
 
-  double E=0.1; //Energía en MeV
-  double dE=0.1;
-  double k=sqrt(E);
-  double beta=sqrt(U-E);
-  double eps=0.0;
+  double E=0.05; //Energía en MeV
+  double dE=0.05;
+  double eps=1.0;
   
-  do
+  while (E<U)
     {
       std::cout<<E<<"\t"<<eqtan(E)<<"\t"<<eqtanh(E)<<"\n";
       E+=dE;
-      k=sqrt(E);
-      beta=sqrt(U-E);
       eps=eqtan(E)-eqtanh(E);
     }
-  while (abs(eps)>=1e-05);
+  
   return 0;
   
 }
@@ -43,15 +38,18 @@ int main ()
 double  eqtan (double E)
 {
   double eq1=0.0;
-  double rE=sqrt(E);
-  eq1=-(1.0/rE)*tan((a*rE)/2.0);
+  double k=sqrt(2*m*E/(hb*hb));
+  double arg=(k/2.0)*(L-a);
+  eq1=(1.0/E)*tan(arg);
   return eq1;
 }
 
 double eqtanh (double E)
 {
-  double eq2=0.0;
-  double m=sqrt(U-E);
-  eq2=(1.0/m)*tanh((m*a)/4.0);
+  double eq2 = 0.0;
+  double k = sqrt(2*m*E/(hb*hb));
+  double beta = sqrt((2*m*U/(hb*hb))-k*k);
+  double arg = beta*L/2;
+  eq2=(1.0/(U-E))*tanh(arg);
   return eq2;
 }
